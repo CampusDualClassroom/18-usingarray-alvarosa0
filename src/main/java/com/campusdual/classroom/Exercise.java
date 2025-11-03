cb6ec3c38cc53f44b9524644eb7afc073374d840
Alvaro Sainz <alvaroalonsovp@outlook.es>
2025-11-03 12:39:00 +0100

    Exercise 18 solved

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -3,42 +3,31 @@ package com.campusdual.classroom;
 import com.campusdual.Utils;
 
 public class Exercise {
-    public static int[] createAndInitializeArray(int size) {
-        if (size <= 0) {
-            return new int[0];
-        }
-
-        int[] array = new int[size];
-
-        for (int i = 0; i < size; i++) {
-            array[i] = Utils.integer("Introduce el elemento " + (i + 1) + " de " + size + ": ");
-        }
-
-        return array;
-    }
-
+    public static int[] createAndInitializeArray(int num) {
+       int [] array = new int[num];
+       for (int i = 0; i < array.length; i++) {
+           array[i] = i + 1;
+       } return array;
+       }
 
     public static void showInlineArray(int[] array) {
-        if (array == null || array.length == 0) {
-            System.out.println("(array vacío)");
-            return;
-        }
-
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i]);
             if (i < array.length - 1) {
                 System.out.print(" ");
             }
+
         }
-        System.out.println();
     }
 
     public static void main(String[] args) {
-        int size = Utils.integer("¿Cuántos elementos tendrá el array? ");
-        int[] miArray = createAndInitializeArray(size);
+        int num = Utils.integer("Introduce el tamaño del array: ");
+        int[] array = createAndInitializeArray(num);
 
-        System.out.print("Array introducido: ");
-        showInlineArray(miArray);
+         showInlineArray(array);
+
+
+
     }
 
 }
